8dff977fe3393766cde261f7f7ac70a61cae9688
Denton <Denton24646@gmail.com>
2017-12-16 14:21:53 -0500

    update functionality to new core
    update functionality to new core

--- core/listener/listener_main.py
+++ core/listener/listener_main.py
@@ -3,13 +3,17 @@ from core.markets import exchange
 from core.database import connection_manager
 
 
+
 #Step one create price database
 connection_manager.reset_db() #for debug
 
 #Step two initialize exchange class
 ETH_BTC_Exchange = exchange.Market('bittrex', 'ETH', 'BTC')
 
-#Step three write data into database
+#Step three create all needed tables
+connection_manager.create_tables()
+
+#Step four write data into database
 ETH_BTC_Exchange.pull_OHLCV_data(59, '1m')
 
 
