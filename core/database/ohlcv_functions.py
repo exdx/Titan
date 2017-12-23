1f068330f89dea26b85617ec8fcf96795c40fbba
Denton <Denton24646@gmail.com>
2017-12-23 15:28:44 -0500

    improved functions
    improved get_latest_candle() and has_candle()

--- core/database/ohlcv_functions.py
+++ core/database/ohlcv_functions.py
@@ -1,10 +1,11 @@
 from core.database import connection_manager
-from sqlalchemy.sql import select
+from sqlalchemy.sql import select, and_
 
 engine = connection_manager.engine
 conn = engine.connect()
 
 def insert_data_into_ohlcv_table(exchange, pair, interval, candle):
+    '''Inserts exchange candle data into table'''
     args = [exchange, pair, candle[0], candle[1], candle[2], candle[3], candle[4], candle[5], interval]
     ins = connection_manager.OHLCV.insert().values(Exchange=args[0], Pair=args[1], Timestamp=args[2], Open=args[3], High=args[4], Low=args[5], Close=args[6],Volume=args[7],Interval=args[8])
     conn.execute(ins)
@@ -12,20 +13,23 @@ def insert_data_into_ohlcv_table(exchange, pair, interval, candle):
 
 
 def get_latest_candle(exchange, pair, interval):
-    s = select([connection_manager.OHLCV]).order_by(connection_manager.OHLCV.ID.desc()).limit(1)
-    args = (exchange, pair, interval,)
+    '''Returns only latest candle if it exists, otherwise returns 0'''
+    s = select([connection_manager.OHLCV]).where(and_(connection_manager.OHLCV.c.Exchange == exchange,connection_manager.OHLCV.c.Pair == pair,connection_manager.OHLCV.c.Interval == interval)).order_by(connection_manager.OHLCV.c.ID.desc()).limit(1)
     result = conn.execute(s)
     row = result.fetchone()
+    print(row)
     return row if (row != None) else 0
 
 
 def has_candle(candle_data, exchange, pair, interval):
+    '''Checks to see if the candle is already in the historical dataset pulled'''
     print('Checking for candle with timestamp: ' + str(candle_data[0]))
     args = (exchange, pair, interval,)
 
-    s = select([connection_manager.OHLCV])
-    candles = conn.execute(s)
-    for row in candles: # now that we are using multiple listeners, loop through all candles (will need to refactor for optimization to loop through only a number of the latest)
+    s = select([connection_manager.OHLCV]).where(and_(connection_manager.OHLCV.c.Exchange == exchange,connection_manager.OHLCV.c.Pair == pair,connection_manager.OHLCV.c.Interval == interval))
+    result = conn.execute(s)
+
+    for row in result: # now that we are using multiple listeners, loop through all candles (will need to refactor for optimization to loop through only a number of the latest)
         if row[3] == candle_data[0] and row[1] == exchange and row[2] == pair:  # compare timestamp of database row to timestamp of candle data passed in
             return True
     return False
@@ -33,4 +37,5 @@ def has_candle(candle_data, exchange, pair, interval):
 
 def convert_timestamp_to_date():
     pass
+
 
