3f7ed179bee26f5ec8732f0eb460418301793fc9
Denton <Denton24646@gmail.com>
2017-12-27 00:18:05 -0500

    optimized ta class to one instance
    optimized ta class to one instance

--- core/__main__.py
+++ core/__main__.py
@@ -13,16 +13,17 @@ def main():
 
     print("Loading Candles...")
 
-    ETH_BTC_Exchange.apply_indicator(ta_functions.SimpleMovingAverage("5m", 1440, ETH_BTC_Exchange, 'slow'))
-    ETH_BTC_Exchange.apply_indicator(ta_functions.SimpleMovingAverage("5m", 12, ETH_BTC_Exchange, 'fast'))
+    ETH_BTC_Exchange.apply_indicator(ta_functions.SimpleMovingAverage(ETH_BTC_Exchange, "5m", 1440, 12))
 
     # running this 'ticker' from the main loop to trigger listeners to pull candles each minute
     live_tick_count = 0
     while True:
         """Running this 'ticker' from the main loop to trigger listeners to pull candles each minute"""
         """Only 55 seconds to stay ahead of the clock (since we have logic to assure no duplicates added)"""
-        print("Live Tick: " + str(live_tick_count))
+        print("Live Tick: {}".format(str(live_tick_count)))
         exchange.update_all_candles(live_tick_count)
+
+        print('Wrote live candle #{} to DB and calculated moving average. Waiting for next live candle...'.format(live_tick_count))
 
         live_tick_count += 1
 
@@ -35,6 +36,9 @@ try:
 
     # run main
     main()
+
+except Exception as e:
+    print(e)
 
 finally:
     connection_manager.engine.dispose()
--- ta/ta_functions.py
+++ ta/ta_functions.py
@@ -10,50 +10,48 @@ lock = Lock()
 
 
 class SimpleMovingAverage:
-    def __init__(self, interval, periods, market, speed):
+    def __init__(self, market, interval, slow_periods, fast_periods):
         self.interval = interval
-        self.periods = periods
         self.market = market
-        self.speed = speed
+        self.slow_periods = slow_periods
+        self.fast_periods = fast_periods
         self.write_strategy_description_to_db()
 
     def next_calculation(self):
         """get latest N candles from market, do calculation, write results to db"""
-        self.dataset = ohlcv_functions.get_latest_N_candles_as_df(self.market.exchange.id, self.market.analysis_pair, self.interval, self.periods)
+        self.slow_dataset = ohlcv_functions.get_latest_N_candles_as_df(self.market.exchange.id, self.market.analysis_pair, self.interval, self.slow_periods)
+        self.fast_dataset = ohlcv_functions.get_latest_N_candles_as_df(self.market.exchange.id, self.market.analysis_pair, self.interval, self.fast_periods)
         self.do_calculation()
         self.write_ta_statistic_to_db()
 
     def calculate_historical(self):
         """do calculations on historical market data if there are enough candles to do the calculation - otherwise return NA"""
-        self.dataset = ohlcv_functions.get_latest_N_candles_as_df(self.market.exchange.id, self.market.analysis_pair,self.market.interval, self.periods)
-        if len(self.dataset['Close'].tolist()) < self.periods:
-            self.sma_result = 'NA'
+        self.slow_dataset = ohlcv_functions.get_latest_N_candles_as_df(self.market.exchange.id, self.market.analysis_pair,self.market.interval, self.slow_periods)
+        if len(self.slow_dataset['Close'].tolist()) < self.slow_periods:
+            self.sma_slow_result = 'NA'
+            self.sma_fast_result = 'NA'
         else:
             self.do_calculation()
 
         self.write_ta_statistic_to_db()
 
     def do_calculation(self):
-        self.sma_result = sma(self.dataset['Close'].tolist(), self.periods)[-1]
-        self.close = self.dataset['Close'].tolist()[0]
-        self.timestamp = self.dataset['Timestamp'].tolist()[0]
+        self.sma_slow_result = sma(self.slow_dataset['Close'].tolist(), self.slow_periods)[-1]
+        self.sma_fast_result = sma(self.fast_dataset['Close'].tolist(), self.fast_periods)[-1]
+        self.close = self.slow_dataset['Close'].tolist()[0]
+        self.timestamp = self.slow_dataset['Timestamp'].tolist()[0]
 
     def write_ta_statistic_to_db(self):
-        '''Inserts average into table, depending on the column whether it is the slow moving average or the fast moving average'''
+        """Inserts average into table, depending on the column whether it is the slow moving average or the fast moving average"""
         with lock:
-            if self.speed == 'slow':
-                ins = connection_manager.TAMovingAverage.insert().values(Pair=self.market.analysis_pair,Time=self.timestamp,Close=self.close,MA_SLOW_INTERVAL=self.periods,MA_SLOW=self.sma_result)
+                ins = connection_manager.TAMovingAverage.insert().values(Pair=self.market.analysis_pair,Time=self.timestamp,Close=self.close,MA_SLOW_INTERVAL=self.slow_periods,MA_SLOW=self.sma_slow_result,MA_FAST_INTERVAL=self.fast_periods,MA_FAST=self.sma_fast_result)
                 conn.execute(ins)
-                print('Writing Slow SMA statistic to db...')
-            elif self.speed == 'fast':
-                ins = connection_manager.TAMovingAverage.insert().values(Pair=self.market.analysis_pair,Time=self.timestamp,Close=self.close,MA_FAST_INTERVAL=self.periods,MA_FAST=self.sma_result)
-                conn.execute(ins)
-                print('Writing Fast SMA statistic to db...')
+                print('Wrote statistic to db...')
 
     def write_strategy_description_to_db(self):
         '''Add ID and description to TAIdentifier table'''
         with lock:
-            ins = connection_manager.TAIdentifier.insert().values(TA_ID=1, Description='A basic SMA Crossover Strategy: {}'.format(self.speed))
+            ins = connection_manager.TAIdentifier.insert().values(TA_ID=1, Description='A basic SMA Crossover Strategy')
             conn.execute(ins)
 
     def convert_periods_to_time(self):
